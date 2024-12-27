876774a45d71c39de34787f4326e89394c0eb398
Renx24 <remerida@hotmail.com>
2024-12-27 16:07:05 +0200

    cleaned up

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,14 +8,9 @@ namespace Calculator
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CalculatorForm());
         }
     }
